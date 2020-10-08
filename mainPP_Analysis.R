29f4b05bf13d656f07c03dce45ad21ebeee15505
jwongala <jennwongala@gmail.com>
2020-10-08 11:05:47 -0700

    This file is currently an empty shell that will eventually be the main script used to post-process and analyze the LPT model output

---
+++ mainPP_Analysis.R
@@ -0,0 +1,6 @@
+### Jennifer Wong-Ala 
+### Date last worked on: 10/08/2020
+### Purpose: The purpose of this script is to be a one stop shop for all data post processing, analysis, and creating figures for the analyses
+### - this will be used to "clean" the raw LPT model output, and analyze the LPT model data and create figures from the Post_Process_LPT_Model_Output, Dispersal_and_Retention_Analyses, SR_test, ConnectivityMatrices, and Data_comparison_insitu_ROMS
+
+
